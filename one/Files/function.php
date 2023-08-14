4b35d17b62224173848ff7ebaca5982620fb102c
Jatin <111072851+TheJatinVaghela@users.noreply.github.com>
2023-08-14 14:54:18 +0530

    modified Function.php

--- one/Files/function.php
+++ one/Files/function.php
@@ -10,6 +10,16 @@ function processmarks($arr): int {
     return $sum;
 };
 
+function avgMarks($arr) : float {
+    $avg_Marks = 0;
+    $i = 0;
+    foreach ($arr as $key => $value) {
+        $avg_Marks += $value;
+        $i++;
+    };
+    return $avg_Marks / $i;
+}
+
 $student = ["rohan","jatin"];
 $rohan = [32,56,35,78,54,79,34];
 $jatin = [36,59,36,74,57,74,36];
@@ -17,6 +27,9 @@ $jatin = [36,59,36,74,57,74,36];
 foreach ($student as $key => $name) {
     $sum = processMarks($$name);
     echo "$name's total marks out of 700 is= $sum"."<br>";
+
+    $avg_Marks = avgMarks($$name);
+    echo "$name's avg marks is $avg_Marks"."<br>";
 }
 // $sum = processMarks($rohan);
 // $sum = processMarks($jatin);

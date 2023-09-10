6f1a229735694ab30b029df367995d9de2b2d625
loljaar <vaghelajatin5@example.com>
2023-09-10 10:16:08 +0530

    Supper globles

---
+++ one/Files/PHP Assignments/MODULE 3 CORE PHP.php
@@ -0,0 +1,42 @@
+<!--
+//01
+ Write a PHP program to enter marks of five subjects Physics, Chemistry,  
+Biology, Mathematics and Computer, calculate percentage and grade by if 
+else write a PHP program for find „Thursday‟ in week using switch 
+Function. -->
+<?php
+/*
+$subjects = [["Physics",0],["Chemistry",0],["Biology",0],["Mathematics",0],["Computer",0]];
+$total=0;
+$percentage;
+// print_r($subjects);
+ for ($i=0; $i < sizeof($subjects); $i++) { 
+     print_r($subjects[$i][0]." add marks ")  ;
+     $input = readline();
+     $subjects[$i][1]=$input;
+     $total += $subjects[$i][1];
+ };
+$percentage = $total / sizeof($subjects) / 10;
+// print_r($subjects);
+// print_r($percentage);
+if ($percentage >= 8 ) {
+    print_r ("A+ grade ".$percentage*10);
+}elseif($percentage >= 4 && $percentage < 8 ) {
+    print_r ("B+ grade ".$percentage*10);
+}elseif($percentage >= 2 && $percentage < 4 ) {
+    print_r ("c+ grade ".$percentage*10);
+}
+
+echo "write a PHP program for find „Thursday‟ in week using switch Function.";
+$weeks = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
+foreach ($weeks as $key => $value) {
+    switch ($value) {
+          case "Thursday":
+            echo "Thursday";
+            break;
+     }
+}
+*/
+
+
+?> 

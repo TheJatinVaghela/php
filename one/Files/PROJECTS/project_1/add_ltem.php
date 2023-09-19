35190d634803a0518854bef9ba9839fd3cfa5d0f
loljaar <vaghelajatin5@example.com>
2023-09-19 12:47:25 +0530

    adding

--- one/Files/PROJECTS/project_1/add_ltem.php
+++ one/Files/PROJECTS/project_1/add_ltem.php
@@ -8,7 +8,7 @@
     print_r ($_REQUEST);
     print_r ($_FILES);
     echo "</pre>";
-      move_uploaded_file($_FILES["item_image"]["name"],"./Save_img/".uniqid().time().$_FILES["item_image"]["tmp_name"]);
+      move_uploaded_file($_FILES["item_image"]["tmp_name"],"./Save_img/".$_FILES["item_image"]["tmp_name"]);
       $_SESSION["product"][] = ["name"=>$_REQUEST["item_name"],"description"=>$_REQUEST["item_description"],
                                 "price"=>$_REQUEST["item_price"],"item_quantity"=>$_REQUEST["item_quantity"],
                                 "item_category"=>$_REQUEST["item_category"],

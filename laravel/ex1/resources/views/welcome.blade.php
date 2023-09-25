8f188d5b264de61896b489e010100c7f2e422f86
loljaar <vaghelajatin5@example.com>
2023-09-25 15:23:47 +0530

    learn

--- laravel/ex1/resources/views/welcome.blade.php
+++ laravel/ex1/resources/views/welcome.blade.php
@@ -5,9 +5,33 @@
     <meta name="viewport" content="width=\, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+
 </head>
 <body>
     <a href="{{route("POST_PAGE")}}">post page</a>
-    {{}}
+    {{-- {{THis is comment}} --}}
+    {{"HELLOW WORD"}}
+    <br>
+    {{ 1 + 5 }}
+    {{-- {{"<h1> this willl not work</h1>"}} --}}
+    {{-- {{"<script> consolelog("this will not work"); </script>" }} --}}
+    {!!"<h1> this willl work</h1>"!!}
+    {!!"<script>console.log('this will work')</script>"!!}
+
+    @php
+        echo "This is how to write PHP here";
+        $loop_values = ["index", "itration", " remainings","count","first", "last", "even", "odd", "depth", "perent"];
+    @endphp
+    <ul>
+        @foreach ($loop_values as $key =>  $value)
+
+            @if ($loop->odd)
+            <li >its ODD => {{ "iteration = ".$loop->iteration .",   key = ".$key." = " .",   value = ". $value . ",  index = ". $loop->index}}</li>
+            @elseif($loop->even)
+            <li>its EVEN => {{"iteration = ".$loop->iteration .",   key = ".$key." = " .",   value = ". $value . ",  index = ". $loop->index}}</li>
+            @endif
+
+        @endforeach
+    </ul>
 </body>
 </html>

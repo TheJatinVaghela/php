09debbccd947ec4da2af8f2aea20b6b62fa16c44
loljaar <vaghelajatin5@example.com>
2024-03-20 11:13:03 +0530

    Last Assignment WorkiOn

---
+++ one/Files/Assessment_PHP - Laravel - Using Controllers and Routes for URLs and APIs - Industry/menagemenent/config/views.php
@@ -0,0 +1,18 @@
+<?php
+// config/views.php
+
+return [
+    'views' => [
+        'headerView',
+        'footerView',
+        'homeView',
+        'addMemberView',
+        'addSecurityView',
+        'showMemberView',
+        'showSecurityView',
+        'loginView',
+        'forgetPasswordView',
+    ],
+];
+
+
